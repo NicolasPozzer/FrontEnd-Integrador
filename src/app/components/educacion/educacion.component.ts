f1297a838bb00b81a6c94002135009f81fe398af
NicolasPozzer <pozzernico@gmail.com>
2023-04-17 05:15:07 -0300

    seccion educacion y experiencia agregados

--- src/app/components/educacion/educacion.component.ts
+++ src/app/components/educacion/educacion.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.css']
 })
-export class EducacionComponent {
+export class EducacionComponent implements OnInit {
 
+  educacion: any[]; // se inicializa la variable
+
+  constructor(private personaService: PersonaService) {
+    this.educacion = [];
+  }
+
+  ngOnInit(): void {
+    this.personaService.getDatos().subscribe(datos => {
+      this.educacion = datos.educacion;
+    });
+  }
 }
--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.css']
 })
-export class ExperienciaComponent {
+export class ExperienciaComponent implements OnInit {
 
+  experiencia: any[]; // se inicializa la variable
+
+  constructor(private personaService: PersonaService) {
+    this.experiencia = [];
+  }
+
+  ngOnInit(): void {
+    this.personaService.getDatos().subscribe(datos => {
+      this.experiencia = datos.experiencia;
+    });
+  }
 }
