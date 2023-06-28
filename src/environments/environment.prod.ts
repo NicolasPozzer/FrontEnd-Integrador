3aa4b03310eb255835b67b99c7fde4150fd1967a
NicolasPozzer <pozzernico@gmail.com>
2023-06-27 21:59:54 -0300

    new-fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
+
 export const environment = {
 
     production: true,
     URL: 'https://tuma15.onrender.com/'
   };
+
+/* Para desarrollo
+  export const environment = {
+
+    production: true,
+    URL: 'http://localhost:8080/'
+  };
+  */
